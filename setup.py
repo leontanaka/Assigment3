f88dd89a2ceb2a1fe01810ffdb70caa77c5de18b
lly00412 <lchen39@stevens.edu>
2020-04-09 16:37:28 -0400

    hw3

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from glob import glob
 
 from setuptools import find_packages
 
-setup(name='Homework3',
+setup(name='Homework1',
       version='1.0',
       description='Python Distribution Utilities',
-      author='Resheng Wang',
+      author='Sergul Aydore',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
